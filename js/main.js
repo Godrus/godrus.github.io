092445da6fd171a8ab7c195ab562d21a3f0276a7
Godrus <sanya-fed_9@mail.ru>
2021-04-15 05:56:21 +0300

    added dark-theme on main & login page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Добавление обработчиков событий
+const toggleSwitch = document.querySelector('.daynight input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// Сохранение предпочтения пользователя для будущих посещений
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
+
+// Проверка сохраненных пользовательских настроек, если таковые имеются, при загрузке веб-сайта.
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
