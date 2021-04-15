46d1d7d41df7d74ce21c3664cd656471e8602bd9
Godrus <sanya-fed_9@mail.ru>
2021-04-15 19:40:22 +0300

    Fix daylight switcher on main page.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,17 @@
 // Добавление обработчиков событий
 const toggleSwitch = document.querySelector('.daynight input[type="checkbox"]');
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }    
-}
 toggleSwitch.addEventListener('change', switchTheme, false);
 
 // Сохранение предпочтения пользователя для будущих посещений
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+function switchTheme({target}) {
+    if (target.checked) {
+        document.documentElement.dataset.theme = 'dark';
         localStorage.setItem('theme', 'dark'); //add this
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.dataset.theme = 'light';
         localStorage.setItem('theme', 'light'); //add this
-    }    
+    }
 }
 
 // Проверка сохраненных пользовательских настроек, если таковые имеются, при загрузке веб-сайта.
@@ -31,5 +22,27 @@ if (currentTheme) {
 
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
+    }
+}
+
+function sendGET() {
+
+    // Отправка запроса и получение ответа
+    // 1. Создаем новый объект XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+
+    // 2. Конфигурируем его: GET-запрос на URL
+    xhr.open('get', 'https://httpbin.org/get', false);
+
+    // 3. Отсылаем заапрос
+    xhr.send();
+
+    // 4. Если код ответа сервера не 200, то это ошибка
+    if (xhr.status != 200) {
+        // обработать ошибку
+        alert(xhr.status + ': ' + xhr.statusText)
+    } else {
+        // вывести результат
+        alert(xhr.responseText);
     }
 }
