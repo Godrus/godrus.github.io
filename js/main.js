8639fe833d251a367fe0b56279f14855bf362cf1
Godrus <sanya-fed_9@mail.ru>
2021-04-21 15:10:30 +0300

    Added Table page and functions fot work with it. Improved the daylight template and css.

--- js/main.js
+++ js/main.js
@@ -94,12 +94,9 @@ function sendGET() {
         xhr.onload = function(){
             alert('Загружено:' + xhr.status + '<br>' + xhr.response);
         };
-
     };
 
-
     const queryString = window.location.search;
-
     console.log(queryString);
 
     // 2. Конфигурируем его: GET-запрос на URL
@@ -108,3 +105,20 @@ function sendGET() {
     // 3. Отсылаем заапрос
     xhr.send();
 }
+
+
+! function(e, t) {
+  "object" == typeof exports && "undefined" != typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define(t) : (e = e || self).listree = t()
+}(this, (function() {
+  "use strict";
+  return function() {
+    const e = document.getElementsByClassName("listree-submenu-heading");
+    Array.from(e).forEach((function(e) {
+      e.classList.add("collapsed"), e.nextElementSibling.style.display = "none", e.addEventListener("click", (function(t) {
+        t.preventDefault();
+        const n = t.target.nextElementSibling;
+        "none" == n.style.display ? (e.classList.remove("collapsed"), e.classList.add("expanded"), n.style.display = "block") : (e.classList.remove("expanded"), e.classList.add("collapsed"), n.style.display = "none"), t.stopPropagation()
+      }))
+    }))
+  }
+}));
