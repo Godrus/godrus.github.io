f9e26d77e6a8b5e2d6a3105ad961dd91c74b2917
Godrus <sanya-fed_9@mail.ru>
2021-04-29 05:54:28 +0300

    Bug fixes. Redesign list tree. Added data page

--- js/main.js
+++ js/main.js
@@ -5,26 +5,27 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 
 // Сохранение предпочтения пользователя для будущих посещений
 function switchTheme({
-    target
+  target
 }) {
-    if (target.checked) {
-        document.documentElement.dataset.theme = 'dark';
-        localStorage.setItem('theme', 'dark'); //add this
-    } else {
-        document.documentElement.dataset.theme = 'light';
-        localStorage.setItem('theme', 'light'); //add this
-    }
+  if (target.checked) {
+    document.documentElement.dataset.theme = 'dark';
+    localStorage.setItem('theme', 'dark'); //add this
+  } else {
+    document.documentElement.dataset.theme = 'light';
+    localStorage.setItem('theme', 'light'); //add this
+  }
 }
 
 // Проверка сохраненных пользовательских настроек, если таковые имеются, при загрузке веб-сайта.
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+
 }
 
 function showTableElement(row) {
@@ -36,10 +37,12 @@ function showTableElement(row) {
   }
 }
 
-function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("myTable");
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("mainTable");
   switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc";
   /*Make a loop that will continue until
   no switching has been done:*/
   while (switching) {
@@ -53,13 +56,22 @@ function sortTable() {
       shouldSwitch = false;
       /*Get the two elements you want to compare,
       one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-      //check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
       }
     }
     if (shouldSwitch) {
@@ -67,57 +79,65 @@ function sortTable() {
       and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount++;
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
     }
   }
 }
 
 function sendGET() {
 
-    // Отправка запроса и получение ответа
-    // 1. Создаем новый объект XMLHttpRequest
-    const xhr = new XMLHttpRequest(); // Инициаализация XMLHttpRequest, отвечает за вызов AJAX
-    // При изменении состояния onreadystatechange вызов функции
-    xhr.onreadystatechange = function () {
-        /*
-        if (xhr.readyState === 4) { // Если код ответа 4, то зпрос завершен и был получен ответ
-            if (xhr.status === 200) { // Если 200, то нет ошибки
-                // вывести результат
-                alert(xhr.status + ': ' + xhr.responseText);
-            } else {
-                // обработать ошибку
-                alert(xhr.status + ': ' + xhr.statusText);
-            }
-        }*/
-        xhr.onload = function(){
-            alert('Загружено:' + xhr.status + '<br>' + xhr.response);
-        };
+  // Отправка запроса и получение ответа
+  // 1. Создаем новый объект XMLHttpRequest
+  const xhr = new XMLHttpRequest(); // Инициаализация XMLHttpRequest, отвечает за вызов AJAX
+  // При изменении состояния onreadystatechange вызов функции
+  xhr.onreadystatechange = function () {
+    /*
+    if (xhr.readyState === 4) { // Если код ответа 4, то зпрос завершен и был получен ответ
+        if (xhr.status === 200) { // Если 200, то нет ошибки
+            // вывести результат
+            alert(xhr.status + ': ' + xhr.responseText);
+        } else {
+            // обработать ошибку
+            alert(xhr.status + ': ' + xhr.statusText);
+        }
+    }*/
+    xhr.onload = function () {
+      alert('Загружено:' + xhr.status + '<br>' + xhr.response);
     };
+  };
 
-    const queryString = window.location.search;
-    console.log(queryString);
+  const queryString = window.location.search;
+  console.log(queryString);
 
-    // 2. Конфигурируем его: GET-запрос на URL
-    xhr.open('get', 'https://ghibliapi.herokuapp.com/films', false); // https://httpbin.org/get https://webhook.site/19fd8d70-e96d-4b58-bb95-d519cee89cad
-    console.log(xhr.statusText);
-    // 3. Отсылаем заапрос
-    xhr.send();
+  // 2. Конфигурируем его: GET-запрос на URL
+  xhr.open('get', 'https://ghibliapi.herokuapp.com/films', false); // https://httpbin.org/get https://webhook.site/19fd8d70-e96d-4b58-bb95-d519cee89cad
+  console.log(xhr.statusText);
+  // 3. Отсылаем заапрос
+  xhr.send();
 }
 
 // Tree View functions
 function tree_toggle(event) {
   event = event || window.event;
   var clickedElem = event.target || event.srcElement;
-
-  if (!hasClass(clickedElem, 'TV-expand')) { return; }
+  if (!hasClass(clickedElem, 'expand')) { return; }
 
   var node = clickedElem.parentNode;
-  if (hasClass(node, 'TV-leaf')) { return; }
+  if (hasClass(node, 'leaf')) { return; }
 
-  var newClass = hasClass(node, 'TV-open') ? 'TV-closed' : 'TV-open';
-  var re = /(^|\s)(TV-open|TV-closed)(\s|$)/;
+  var newClass = hasClass(node, 'open') ? 'closed' : 'open';
+  var re = /(^|\s)(open|closed)(\s|$)/;
   node.className = node.className.replace(re, '$1' + newClass + '$3');
 }
 
-function hasClass(elem, className){ 
+function hasClass(elem, className) {
   return new RegExp("(^|\\s)" + className + "(\\s|$)").test(elem.className);
 }
